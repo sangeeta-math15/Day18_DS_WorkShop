886d69332e3e6dd330165d787b34832e8add5b91
sngtmth <sngtmth@gmail.com>
2021-10-24 18:36:47 +0530

    Added:Welcome Message

---
+++ src/com/bridgelabz/workshop/DeckOfCard.java
@@ -0,0 +1,10 @@
+/**
+ * Program for Deck Of Card using oops concept
+ */
+package com.bridgelabz.workshop;
+
+public class DeckOfCard {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DeckOfCard Program");
+    }
+}
